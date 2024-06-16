9e324d4c50de663b050b9d714c7b224da2b64ef3
rafaelRueda <rrrm85@gmail.com>
2024-06-15 19:33:14 -0500

    Add alien fleet and down motion

--- settings.py
+++ settings.py
@@ -15,5 +15,11 @@ class Settings:
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 160, 60)
+        self.bullet_color = (160, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right and -1 represents left
+        self.fleet_direction = 1
